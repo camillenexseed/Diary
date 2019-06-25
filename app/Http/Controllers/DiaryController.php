72e74a72060e6035097141b0891e909dfbc30af1
nexseedcamille <ykamimori@nexseed.net>
2019-06-25 19:26:11 +0800

    Diaryコントローラーにロジックを追加#3
    Diaryコントローラーにデータベースを引き出せるようにコード追加。

--- app/Http/Controllers/DiaryController.php
+++ app/Http/Controllers/DiaryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Diary; // App/Diaryクラスを使用する宣言
 use Illuminate\Http\Request;
 
 class DiaryController extends Controller
@@ -9,6 +10,13 @@ class DiaryController extends Controller
     // 追加
     public function index()
     {
-        return 'Hello World';
+        // return 'Hello World';
+
+        //diariesテーブルのデータを全件取得
+        //useしてるDiaryのallメソッドを実施
+        //all()はテーブルのデータを全て取得するメソッド
+        $diaries = Diary::all();
+
+        dd($diaries);  //var_dump()とdie()を合わせたメソッド。変数の確認 + 処理のストップ
     }
 }
